34fba33ff652620f2b93e127978994424c3b4dd9
Gbonn7 <gbonninlettering@gmail.com>
2024-05-07 17:26:59 +0200

    Emprolijado script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,29 @@
 'use strict';
 
-const buttonSend = document.querySelector("button");
+const buttonSend = document.querySelector('button');
 
-function doSomething(latitude, longitude) {
-    console.log(latitude, longitude);
-  };
+buttonSend.addEventListener('click', () => {
+  console.log('click');
 
-buttonSend.addEventListener('click',()=>{
-    console.log('click');
+  if ('geolocation' in navigator) {
+    //Obtenemos la geolocalización
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      //Realizamos una petición a la API
+      const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=precipitation,rain&forecast_days=1&forecast_hours=8`
+      );
 
-    if ('geolocation' in navigator) {
-        console.log('funciona');
-        navigator.geolocation.getCurrentPosition((position) => {
-            doSomething(position.coords.latitude, position.coords.longitude);
-          });
-          fetch('https://api.open-meteo.com/v1/forecast?latitude=43.4799&longitude=-8.2182&hourly=precipitation,rain&forecast_days=1&forecast_hours=8') 
-          .then(function (response){
-              return response.json();
-          })
-          .then(function(data){
-              if (data.hourly.precipitation === true || data.hourly.rain === true) {
-                  console.log('lloverá');
-                  
-              } else {
-                  console.log('no lloverá');
-                  
-              };
-          });
-    } else {
-        console.error('error');
-    }
+      const body = await response.json();
+      console.log(body);
+      if (body.hourly.precipitation === true || body.hourly.rain === true) {
+        console.log('lloverá');
+      } else {
+        console.log('no lloverá');
+      }
+    });
+  } else {
+    console.error('error');
+  }
 });
-
-    
-
-
-
-
