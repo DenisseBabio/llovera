9fe881e6234d6e99d69f59cbfb25121cfe3fdc42
Denisse Babio <denissebabiodoval@gmail.com>
2024-05-09 01:05:38 +0200

    añadiendo más datos y solucionando errores

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-'use strict';
+"use strict";
 
-const buttonSend = document.querySelector('button');
+import { getWeatherIcon } from "/js/helpers.js";
 
-buttonSend.addEventListener('click', () => {
-  console.log('click');
+const buttonSend = document.querySelector("button");
 
-  if ('geolocation' in navigator) {
+//Seleccionamos los elementos del DOM
+const rain = document.querySelector("#rain");
+const noRain = document.querySelector("#noRain");
+const error = document.querySelector("#error");
+const actually = document.querySelector("#actually");
+const nextHours = document.querySelector("#nextHours");
+
+//Hacemos console.log para confirmar que funciona el botón
+buttonSend.addEventListener("click", () => {
+  console.log("click");
+
+  if (navigator.geolocation) {
     // Obtenemos la geolocalización
-
     navigator.geolocation.getCurrentPosition(async (position) => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-
       // Realizamos una petición a la API
       const response = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=rain&forecast_hours=8&hourly=temperature_2m&forecast_hours=8&hourly=weather_code&forecast_hours=8&timeformat`
@@ -43,17 +51,48 @@ buttonSend.addEventListener('click', () => {
         body.hourly.time = hoursArray;
       }
 
+      //Comprobamos si va a llover en las próximas 8 horas y pasamos resultado.
+
       function getRain() {
-        body.hourly.rain.map((rain) => {
-          if (rain > 0.01) {
-            console.log('Va a llover en las próximas 8 horas.');
+        console.log(body.hourly.rain);
+        const rain = body.hourly.rain;
+
+        rain.map((item) => {
+          if (item > 0.01) {
+            console.log("Va a llover en las próximas 8 horas.");
             return true;
           } else {
-            console.log('No va a llover en las próximas 8 horas.');
+            console.log("No va a llover en las próximas 8 horas.");
             return false;
           }
         });
       }
+      
+      //Hacemos que se muestren los datos en pantalla.
+      function showHourlyData() {
+        console.log(body);
+        for (let index = 0; index < 8; index++) {
+          const container = document.createElement("div");
+          container.classList.add("nextHoursContainer");
+          const section = document.createElement("section");
+          const next = document.createElement("h3");
+          const degree = document.createElement("p");
+          const img = document.createElement("img");
+          const hourData = body.hourly.time[index].split("T")[1];
+          next.textContent = `${hourData} h.`;
+
+          img.src = getWeatherIcon(body.hourly.weather_code[index]);
+
+          degree.textContent = `${body.hourly.temperature_2m[index]}ºC`;
+
+          section.appendChild(next);
+          section.appendChild(img);
+          section.appendChild(degree);
+          container.appendChild(section);
+          nextHours.appendChild(container);
+        }
+      }
+      showHourlyData();
 
       getRain();
       console.log(body.hourly.rain);
@@ -61,7 +100,8 @@ buttonSend.addEventListener('click', () => {
       console.log(body.hourly.time);
     });
   } else {
-    console.error('error');
+
+    // Mensaje de error si la ubicación no funciona
+    console.error("error");
   }
 });
-
