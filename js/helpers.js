1d31d46333bc59baaa7a56c8a4fa67172a5938d5
Denisse Babio <denissebabiodoval@gmail.com>
2024-05-10 02:16:55 +0200

    soluciones

--- js/helpers.js
+++ js/helpers.js
@@ -1,319 +1,319 @@
 export const getWeatherIcon = (cod, hourData) => {
-    const iconObject = codeWeather.find((weather) => weather.cod === cod);
-    if (hourData < 21) {
-      return iconObject.day.image;
-    } else {
-      return iconObject.night.image;
-    }
-  };
-  
-  const codeWeather = [
-    {
-      cod: 0,
-      day: {
-        description: "Sunny",
-        image: "http://openweathermap.org/img/wn/01d@2x.png",
-      },
-      night: {
-        description: "Clear",
-        image: "http://openweathermap.org/img/wn/01n@2x.png",
-      },
+  const iconObject = codeWeather.find((weather) => weather.cod === cod);
+  if (hourData >7 && hourData <21) {
+    return iconObject.day.image;
+  } else {
+    return iconObject.night.image;
+  }
+};
+
+const codeWeather = [
+  {
+    cod: 0,
+    day: {
+      description: "Sunny",
+      image: "http://openweathermap.org/img/wn/01d@2x.png",
     },
-    {
-      cod: 1,
-      day: {
-        description: "Mainly Sunny",
-        image: "http://openweathermap.org/img/wn/01d@2x.png",
-      },
-      night: {
-        description: "Mainly Clear",
-        image: "http://openweathermap.org/img/wn/01n@2x.png",
-      },
+    night: {
+      description: "Clear",
+      image: "http://openweathermap.org/img/wn/01n@2x.png",
     },
-    {
-      cod: 2,
-      day: {
-        description: "Partly Cloudy",
-        image: "http://openweathermap.org/img/wn/02d@2x.png",
-      },
-      night: {
-        description: "Partly Cloudy",
-        image: "http://openweathermap.org/img/wn/02n@2x.png",
-      },
+  },
+  {
+    cod: 1,
+    day: {
+      description: "Mainly Sunny",
+      image: "http://openweathermap.org/img/wn/01d@2x.png",
     },
-    {
-      cod: 3,
-      day: {
-        description: "Cloudy",
-        image: "http://openweathermap.org/img/wn/03d@2x.png",
-      },
-      night: {
-        description: "Cloudy",
-        image: "http://openweathermap.org/img/wn/03n@2x.png",
-      },
+    night: {
+      description: "Mainly Clear",
+      image: "http://openweathermap.org/img/wn/01n@2x.png",
     },
-    {
-      cod: 45,
-      day: {
-        description: "Foggy",
-        image: "http://openweathermap.org/img/wn/50d@2x.png",
-      },
-      night: {
-        description: "Foggy",
-        image: "http://openweathermap.org/img/wn/50n@2x.png",
-      },
+  },
+  {
+    cod: 2,
+    day: {
+      description: "Partly Cloudy",
+      image: "http://openweathermap.org/img/wn/02d@2x.png",
     },
-    {
-      cod: 48,
-      day: {
-        description: "Rime Fog",
-        image: "http://openweathermap.org/img/wn/50d@2x.png",
-      },
-      night: {
-        description: "Rime Fog",
-        image: "http://openweathermap.org/img/wn/50n@2x.png",
-      },
+    night: {
+      description: "Partly Cloudy",
+      image: "http://openweathermap.org/img/wn/02n@2x.png",
     },
-    {
-      cod: 51,
-      day: {
-        description: "Light Drizzle",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Light Drizzle",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+  },
+  {
+    cod: 3,
+    day: {
+      description: "Cloudy",
+      image: "http://openweathermap.org/img/wn/03d@2x.png",
     },
-    {
-      cod: 53,
-      day: {
-        description: "Drizzle",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Drizzle",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+    night: {
+      description: "Cloudy",
+      image: "http://openweathermap.org/img/wn/03n@2x.png",
     },
-    {
-      cod: 55,
-      day: {
-        description: "Heavy Drizzle",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Heavy Drizzle",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+  },
+  {
+    cod: 45,
+    day: {
+      description: "Foggy",
+      image: "http://openweathermap.org/img/wn/50d@2x.png",
     },
-    {
-      cod: 56,
-      day: {
-        description: "Light Freezing Drizzle",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Light Freezing Drizzle",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+    night: {
+      description: "Foggy",
+      image: "http://openweathermap.org/img/wn/50n@2x.png",
     },
-    {
-      cod: 57,
-      day: {
-        description: "Freezing Drizzle",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Freezing Drizzle",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+  },
+  {
+    cod: 48,
+    day: {
+      description: "Rime Fog",
+      image: "http://openweathermap.org/img/wn/50d@2x.png",
     },
-    {
-      cod: 61,
-      day: {
-        description: "Light Rain",
-        image: "http://openweathermap.org/img/wn/10d@2x.png",
-      },
-      night: {
-        description: "Light Rain",
-        image: "http://openweathermap.org/img/wn/10n@2x.png",
-      },
+    night: {
+      description: "Rime Fog",
+      image: "http://openweathermap.org/img/wn/50n@2x.png",
     },
-    {
-      cod: 63,
-      day: {
-        description: "Rain",
-        image: "http://openweathermap.org/img/wn/10d@2x.png",
-      },
-      night: {
-        description: "Rain",
-        image: "http://openweathermap.org/img/wn/10n@2x.png",
-      },
+  },
+  {
+    cod: 51,
+    day: {
+      description: "Light Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
     },
-    {
-      cod: 65,
-      day: {
-        description: "Heavy Rain",
-        image: "http://openweathermap.org/img/wn/10d@2x.png",
-      },
-      night: {
-        description: "Heavy Rain",
-        image: "http://openweathermap.org/img/wn/10n@2x.png",
-      },
+    night: {
+      description: "Light Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
     },
-    {
-      cod: 66,
-      day: {
-        description: "Light Freezing Rain",
-        image: "http://openweathermap.org/img/wn/10d@2x.png",
-      },
-      night: {
-        description: "Light Freezing Rain",
-        image: "http://openweathermap.org/img/wn/10n@2x.png",
-      },
+  },
+  {
+    cod: 53,
+    day: {
+      description: "Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
     },
-    {
-      cod: 67,
-      day: {
-        description: "Freezing Rain",
-        image: "http://openweathermap.org/img/wn/10d@2x.png",
-      },
-      night: {
-        description: "Freezing Rain",
-        image: "http://openweathermap.org/img/wn/10n@2x.png",
-      },
+    night: {
+      description: "Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
     },
-    {
-      cod: 71,
-      day: {
-        description: "Light Snow",
-        image: "http://openweathermap.org/img/wn/13d@2x.png",
-      },
-      night: {
-        description: "Light Snow",
-        image: "http://openweathermap.org/img/wn/13n@2x.png",
-      },
+  },
+  {
+    cod: 55,
+    day: {
+      description: "Heavy Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
     },
-    {
-      cod: 73,
-      day: {
-        description: "Snow",
-        image: "http://openweathermap.org/img/wn/13d@2x.png",
-      },
-      night: {
-        description: "Snow",
-        image: "http://openweathermap.org/img/wn/13n@2x.png",
-      },
+    night: {
+      description: "Heavy Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
     },
-    {
-      cod: 75,
-      day: {
-        description: "Heavy Snow",
-        image: "http://openweathermap.org/img/wn/13d@2x.png",
-      },
-      night: {
-        description: "Heavy Snow",
-        image: "http://openweathermap.org/img/wn/13n@2x.png",
-      },
+  },
+  {
+    cod: 56,
+    day: {
+      description: "Light Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
     },
-    {
-      cod: 77,
-      day: {
-        description: "Snow Grains",
-        image: "http://openweathermap.org/img/wn/13d@2x.png",
-      },
-      night: {
-        description: "Snow Grains",
-        image: "http://openweathermap.org/img/wn/13n@2x.png",
-      },
+    night: {
+      description: "Light Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
     },
-    {
-      cod: 80,
-      day: {
-        description: "Light Showers",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Light Showers",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+  },
+  {
+    cod: 57,
+    day: {
+      description: "Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
     },
-    {
-      cod: 81,
-      day: {
-        description: "Showers",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Showers",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+    night: {
+      description: "Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
     },
-    {
-      cod: 82,
-      day: {
-        description: "Heavy Showers",
-        image: "http://openweathermap.org/img/wn/09d@2x.png",
-      },
-      night: {
-        description: "Heavy Showers",
-        image: "http://openweathermap.org/img/wn/09n@2x.png",
-      },
+  },
+  {
+    cod: 61,
+    day: {
+      description: "Light Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
     },
-    {
-      cod: 85,
-      day: {
-        description: "Light Snow Showers",
-        image: "http://openweathermap.org/img/wn/13d@2x.png",
-      },
-      night: {
-        description: "Light Snow Showers",
-        image: "http://openweathermap.org/img/wn/13n@2x.png",
-      },
+    night: {
+      description: "Light Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
     },
-    {
-      cod: 86,
-      day: {
-        description: "Snow Showers",
-        image: "http://openweathermap.org/img/wn/13d@2x.png",
-      },
-      night: {
-        description: "Snow Showers",
-        image: "http://openweathermap.org/img/wn/13n@2x.png",
-      },
+  },
+  {
+    cod: 63,
+    day: {
+      description: "Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
     },
-    {
-      cod: 95,
-      day: {
-        description: "Thunderstorm",
-        image: "http://openweathermap.org/img/wn/11d@2x.png",
-      },
-      night: {
-        description: "Thunderstorm",
-        image: "http://openweathermap.org/img/wn/11n@2x.png",
-      },
+    night: {
+      description: "Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
     },
-    {
-      cod: 96,
-      day: {
-        description: "Light Thunderstorms With Hail",
-        image: "http://openweathermap.org/img/wn/11d@2x.png",
-      },
-      night: {
-        description: "Light Thunderstorms With Hail",
-        image: "http://openweathermap.org/img/wn/11n@2x.png",
-      },
+  },
+  {
+    cod: 65,
+    day: {
+      description: "Heavy Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
     },
-    {
-      cod: 99,
-      day: {
-        description: "Thunderstorm With Hail",
-        image: "http://openweathermap.org/img/wn/11d@2x.png",
-      },
-      night: {
-        description: "Thunderstorm With Hail",
-        image: "http://openweathermap.org/img/wn/11n@2x.png",
-      },
+    night: {
+      description: "Heavy Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
     },
-  ];
+  },
+  {
+    cod: 66,
+    day: {
+      description: "Light Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Light Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  {
+    cod: 67,
+    day: {
+      description: "Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  {
+    cod: 71,
+    day: {
+      description: "Light Snow",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Light Snow",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  {
+    cod: 73,
+    day: {
+      description: "Snow",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Snow",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  {
+    cod: 75,
+    day: {
+      description: "Heavy Snow",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Heavy Snow",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  {
+    cod: 77,
+    day: {
+      description: "Snow Grains",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Snow Grains",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  {
+    cod: 80,
+    day: {
+      description: "Light Showers",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Light Showers",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  {
+    cod: 81,
+    day: {
+      description: "Showers",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Showers",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  {
+    cod: 82,
+    day: {
+      description: "Heavy Showers",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Heavy Showers",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  {
+    cod: 85,
+    day: {
+      description: "Light Snow Showers",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Light Snow Showers",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  {
+    cod: 86,
+    day: {
+      description: "Snow Showers",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Snow Showers",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  {
+    cod: 95,
+    day: {
+      description: "Thunderstorm",
+      image: "http://openweathermap.org/img/wn/11d@2x.png",
+    },
+    night: {
+      description: "Thunderstorm",
+      image: "http://openweathermap.org/img/wn/11n@2x.png",
+    },
+  },
+  {
+    cod: 96,
+    day: {
+      description: "Light Thunderstorms With Hail",
+      image: "http://openweathermap.org/img/wn/11d@2x.png",
+    },
+    night: {
+      description: "Light Thunderstorms With Hail",
+      image: "http://openweathermap.org/img/wn/11n@2x.png",
+    },
+  },
+  {
+    cod: 99,
+    day: {
+      description: "Thunderstorm With Hail",
+      image: "http://openweathermap.org/img/wn/11d@2x.png",
+    },
+    night: {
+      description: "Thunderstorm With Hail",
+      image: "http://openweathermap.org/img/wn/11n@2x.png",
+    },
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,107 +1,109 @@
 "use strict";
 
+// Importamos funcion desde helpers
 import { getWeatherIcon } from "/js/helpers.js";
 
-const buttonSend = document.querySelector("button");
+// Indicamos cuando se debe añadir cada dato
+document.addEventListener('DOMContentLoaded', function() {
+  const buttonSend = document.querySelector("button");
+  const rain = document.querySelector("#rain");
+  const noRain = document.querySelector("#noRain");
+  const errorSection = document.querySelector("#error"); 
+  const actually = document.querySelector("#actually");
+  const nextHours = document.querySelector("#nextHours");
 
-//Seleccionamos los elementos del DOM
-const rain = document.querySelector("#rain");
-const noRain = document.querySelector("#noRain");
-const error = document.querySelector("#error");
-const actually = document.querySelector("#actually");
-const nextHours = document.querySelector("#nextHours");
-
-//Hacemos console.log para confirmar que funciona el botón
-buttonSend.addEventListener("click", () => {
-  console.log("click");
-
-  if (navigator.geolocation) {
-    // Obtenemos la geolocalización
-    navigator.geolocation.getCurrentPosition(async (position) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      // Realizamos una petición a la API
-      const response = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=rain&forecast_hours=8&hourly=temperature_2m&forecast_hours=8&hourly=weather_code&forecast_hours=8&timeformat`
-      );
-
-      const body = await response.json();
-      console.log(body);
-
-      function getHours() {
-        const now = new Date();
-        const currentHour = now.getHours();
-
-        // Creamos un array con el resultado de las horas.
-        const hoursArray = [];
-
-        // Hacemos un bucle que nos muestre las siguientes 8 horas.
-        for (let i = 0; i < 8; i++) {
-          const hour = (currentHour + i) % 24;
-
-          // Creamos un objeto que reinicie las horas cuando llegue a las 00:00
-          const nextHour = new Date(now);
-          nextHour.setHours(hour, 0, 0, 0);
-
-          // Pusheamos el reinicio en el array
-          hoursArray.push(nextHour);
-        }
-
-        // Asignamos los resultados al array fetcheado
-        body.hourly.time = hoursArray;
-      }
-
-      //Comprobamos si va a llover en las próximas 8 horas y pasamos resultado.
-
-      function getRain() {
-        console.log(body.hourly.rain);
-        const rain = body.hourly.rain;
-
-        rain.map((item) => {
-          if (item > 0.01) {
-            console.log("Va a llover en las próximas 8 horas.");
-            return true;
-          } else {
-            console.log("No va a llover en las próximas 8 horas.");
-            return false;
-          }
-        });
-      }
-      
-      //Hacemos que se muestren los datos en pantalla.
-      function showHourlyData() {
-        console.log(body);
-        for (let index = 0; index < 8; index++) {
-          const container = document.createElement("div");
-          container.classList.add("nextHoursContainer");
-          const section = document.createElement("section");
-          const next = document.createElement("h3");
-          const degree = document.createElement("p");
-          const img = document.createElement("img");
-          const hourData = body.hourly.time[index].split("T")[1];
-          next.textContent = `${hourData} h.`;
-
-          img.src = getWeatherIcon(body.hourly.weather_code[index]);
-
-          degree.textContent = `${body.hourly.temperature_2m[index]}ºC`;
-
-          section.appendChild(next);
-          section.appendChild(img);
-          section.appendChild(degree);
-          container.appendChild(section);
-          nextHours.appendChild(container);
-        }
-      }
-      showHourlyData();
-
-      getRain();
-      console.log(body.hourly.rain);
-      getHours();
-      console.log(body.hourly.time);
-    });
-  } else {
-
-    // Mensaje de error si la ubicación no funciona
-    console.error("error");
+  //Limpiamos pantalla
+  function clean() {
+      rain.classList.add("hidden");
+      noRain.classList.add("hidden");
+      errorSection.classList.add("hidden"); 
+      actually.classList.add("hidden");
+      nextHours.classList.add("hidden");
+      document.querySelectorAll(".nextHoursContainer").forEach((el) => {
+          el.remove();
+      });
   }
+  clean(); 
+
+    //Controlamos botón
+    buttonSend.addEventListener("click", () => {
+        console.log("click");
+
+        //Comprobamos ubicacion
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                //Incluimos ubicacion en la API
+                const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=rain,temperature_2m,weather_code&forecast_hours=8`);
+
+                //Convertimos a JSON
+                const body = await response.json();
+                console.log(body);
+
+                //Ajustamos las horas
+                function getHours() {
+                    const now = new Date();
+                    const currentHour = now.getHours();
+
+                    for (let i = 0; i < 8; i++) {
+                        const hour = (currentHour + i) % 24;
+                        const nextHour = new Date(now);
+                        nextHour.setHours(hour, 0, 0, 0);
+                        body.hourly.time[i] = nextHour.toISOString(); 
+                    }
+                }
+
+                //Comprobamos si lloverá o no 
+                function getRain() {
+                    let willRain = body.hourly.rain.some(rainAmount => rainAmount > 0.01);
+                    if (willRain) {
+                      document.getElementById('rain').style.display = 'block';
+                      document.getElementById('noRain').style.display = 'none';
+                    } else {
+                      document.getElementById('rain').style.display = 'none';
+                      document.getElementById('noRain').style.display = 'block';
+                    }
+                    console.log("Rain check: ", willRain ? "Will rain" : "No rain");
+                }
+
+                //Mostramos los datos del clima
+                function showHourlyData() {
+                    for (let index = 0; index < body.hourly.time.length; index++) {
+                        const container = document.createElement("div");
+                        container.classList.add("nextHoursContainer");
+                        const section = document.createElement("section");
+                        const next = document.createElement("h3");
+                        const degree = document.createElement("p");
+                        const img = document.createElement("img");
+
+                        const hourData = new Date(body.hourly.time[index]).getHours() + ":00";
+                        next.textContent = hourData;
+                        img.src = getWeatherIcon(body.hourly.weather_code[index], new Date(body.hourly.time[index]).getHours());
+                        degree.textContent = `${body.hourly.temperature_2m[index]}ºC`;
+
+                        section.appendChild(next);
+                        section.appendChild(img);
+                        section.appendChild(degree);
+                        container.appendChild(section);
+                        nextHours.appendChild(container);
+                    }
+                }
+
+                //Llamamos a las funciones
+                getHours();
+                showHourlyData();
+                getRain();
+
+            //Indicamos errores
+            }, error => {
+                console.error("Geolocation error:", error);
+                error.classList.remove("hidden");
+            });
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+            error.classList.remove("hidden");
+        }
+    });
 });
